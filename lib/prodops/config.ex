812a3cc760afe0c33083a68aa88b5e546ec43e96
Eric Streeper <eric.streeper@revelry.co>
2024-04-26 13:49:39 -0600

    Switch to overridable options

--- lib/prodops/config.ex
+++ lib/prodops/config.ex
@@ -3,25 +3,57 @@ defmodule ProdopsEx.Config do
   Defines the configuration structure for interacting with the ProdOps API.
   """
 
-  defstruct api_url: "https://app.prodops.ai",
-            bearer_token: nil,
-            http_options: [recv_timeout: :infinity]
-
-  @doc """
-  Creates a new Config struct from a keyword list.
-  """
-  def new(opts \\ []) do
-    Enum.into(opts, %__MODULE__{})
+  defp definition do
+    [
+      api_url: [
+        type: :string,
+        default: "https://app.prodops.ai"
+      ],
+      bearer_token: [
+        type: :string,
+        required: true
+      ],
+      http_options: [
+        type: :keyword_list,
+        keys: :*,
+        default: [
+          recv_timeout: 60_000
+        ],
+        required: false
+      ]
+    ]
   end
 
-  definition = [
-    api_url: [
-      type: :string,
-      default: "https://app.prodops.ai"
-    ],
-    bearer_token: [
-      type: :string,
-      required: true
-    ]
-  ]
+  @doc """
+  Return configuration based on passed-in, application, and default
+  values combined.
+
+  ## Precedence
+
+  Configuration is applied with the following precendence (highest to
+  lowest):
+   - configuration passed into this function
+   - application configuration (i.e. via config files)
+   - default configuration
+
+  Usually you do not need to use this function directly, it is already
+  used by methods in this library.
+  """
+  @spec resolve_config(Keyword.t()) :: Keyword.t()
+  def resolve_config(passed_in_config) do
+    application_config = Application.get_all_env(:prodops_ex)
+
+    passed_in_http_options = passed_in_config[:http_options] || []
+    application_http_options = application_config[:http_options] || []
+
+    http_options = Keyword.merge(application_http_options, passed_in_http_options)
+
+    resolved_config =
+      Keyword.new()
+      |> Keyword.merge(application_config)
+      |> Keyword.merge(passed_in_config)
+      |> Keyword.put(:http_options, http_options)
+
+    NimbleOptions.validate!(resolved_config, definition())
+  end
 end
