a352e60109d46e75d1a109a98bf36e11ead045b9
Christopher Stansbury <chris.stansbury@revelry.co>
2024-05-01 16:15:27 -0500

    - mix format

--- test/prodops_ex/artifact_type_test.exs
+++ test/prodops_ex/artifact_type_test.exs
@@ -12,13 +12,19 @@ defmodule ProdopsEx.ArtifactTypeTest do
       api_url = "https://api.example.com"
       api_key = "secret_key"
       full_url = "#{api_url}/api/v1/artifact_types"
-      response = {:ok, %{status: "ok", response: %{"artifact_types" => [
-        %{"slug" => "story", "name" => "Story", "description" => "This is a story"}
-      ]}}}
 
-      Client
-      |> expect(:api_get, fn ^full_url, _config -> response end)
+      response =
+        {:ok,
+         %{
+           status: "ok",
+           response: %{
+             "artifact_types" => [
+               %{"slug" => "story", "name" => "Story", "description" => "This is a story"}
+             ]
+           }
+         }}
 
+      expect(Client, :api_get, fn ^full_url, _config -> response end)
       assert ArtifactType.list(api_url: api_url, api_key: api_key) == response
     end
   end
