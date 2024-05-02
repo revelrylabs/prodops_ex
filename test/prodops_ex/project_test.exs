2c6f0adbd95674cea073962831db6181d48bd342
Christopher Stansbury <chris.stansbury@revelry.co>
2024-05-02 15:18:47 -0500

    - add tests checking client/endpoint correctness for remaining endpoint modules

---
+++ test/prodops_ex/project_test.exs
@@ -0,0 +1,26 @@
+defmodule ProdopsEx.ProjectTest do
+  use ExUnit.Case, async: true
+  use Mimic
+
+  alias ProdopsEx.Project
+  alias ProdopsEx.Client
+
+  describe "list/1" do
+    test "returns a list of all projects for a given team" do
+      config = [api_url: "https://api.example.com", api_key: "secret_key"]
+      full_url = "#{config[:api_url]}/api/v1/projects"
+
+      response = {:ok, %{
+        "status" => "ok",
+        "response" => %{
+          "projects" => [
+            %{"id" => 1, "name" => "ProdOps", "overview" => "This is the project overview"}
+          ]
+        }
+      }}
+
+      expect(Client, :api_get, fn ^full_url, _config -> response end)
+      assert Project.list(config) == response
+    end
+  end
+end
